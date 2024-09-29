82b8ba0ec56951e9e6c042e76d481413fdc47c06
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-09-29 15:32:45 +0600

    before installing next-transpile-modules

--- app/api/deploy/route.ts
+++ app/api/deploy/route.ts
@@ -1,7 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import deploy from "../../utils/deploy";
+import { ethers } from "hardhat";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-
-  return NextResponse.json({ message: "Hello, world!" });
+  console.log("0");
+  try {
+    const [deployer, owner] = await ethers.getSigners();
+    console.log("1");
+    const CoinshitterFactory = await ethers.getContractFactory("Coinshitter");
+    console.log("2");
+    const coinshitterContract = await CoinshitterFactory.deploy();
+    console.log("3");
+    await coinshitterContract.waitForDeployment();
+    // const body = await request.json();
+    // const result = await deploy();
+    // return NextResponse.json({ message: "Deployment successful" });
+  } catch (error: any) {
+    console.error("Deployment error:", error);
+    return NextResponse.json(
+      { message: "Deployment failed", error: error.message },
+      { status: 500 }
+    );
+  }
 }
--- app/utils/deploy.ts
+++ app/utils/deploy.ts
@@ -1,17 +1,18 @@
 import hre, { ethers } from "hardhat";
 
-async function main() {
+const deploy = async () => {
+  //pass contract arguments here
   console.log("DEPLOYING...");
   const [deployer, owner] = await ethers.getSigners();
 
   const CoinshitterFactory = await ethers.getContractFactory("Coinshitter");
   const coinshitterContract = await CoinshitterFactory.deploy();
   await coinshitterContract.waitForDeployment();
-}
+  //get info from deployed contract and return it
+  return {
+    address: coinshitterContract.getAddress(),
+    owner: await coinshitterContract.owner(),
+  };
+};
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+export default deploy;
