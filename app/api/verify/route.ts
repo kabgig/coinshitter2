b1647543a115cebcef812750d04d8f79e1ea6021
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-10-14 12:43:21 +0600

    clean up

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -2,7 +2,6 @@ import { exec } from "child_process";
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import dotenv from "dotenv";
-import hre from "hardhat";
 
 // Load environment variables from .env file
 dotenv.config();
