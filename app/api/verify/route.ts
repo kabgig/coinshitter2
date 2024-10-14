3437d8fd9e0e67e1d7ab0a437b2742de1011643e
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-10-14 23:47:47 +0600

    verbose log level

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       "scripts/verifyer.ts"
     );
     exec(
-      `npx hardhat run ${verificationScriptPath} --network basesepolia`,
+      `npx hardhat run ${verificationScriptPath} --network basesepolia --loglevel=verbose`,
       {
         env: {
           ...process.env,
