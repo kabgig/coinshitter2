f0666754c5547e3671b7a3ea4a1ed50d6f8cc37e
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-10-16 13:48:46 +0600

    fix3

--- scripts/verifyer.ts
+++ scripts/verifyer.ts
@@ -1,4 +1,4 @@
-//import hre from "hardhat";
+import hre from "hardhat";
 
 export default async function main() {
   const deployedContractAddress = process.env.address;
@@ -6,27 +6,23 @@ export default async function main() {
   const totalSupply = process.env.totalSupply;
   const marketingAddress = process.env.marketingAddress;
 
-  // console.log("totalSupply", totalSupply);
-  // console.log("marketingAddress", marketingAddress);
-  // console.log("deployedContractAddress", deployedContractAddress);
-  // console.log("contract", contract);
   if (!deployedContractAddress || !contract || !totalSupply) {
     throw new Error("Missing required environment variables");
   }
   console.log("\nVERIFICATION....");
 
-  // try {
-  //   await hre.run("verify:verify", {
-  //     address: deployedContractAddress,
-  //     constructorArguments: [totalSupply, marketingAddress],
-  //   });
-  //   //на крайняк можно текстовый файл билдить с уникальным название контракта
-  //   console.log(
-  //     JSON.stringify({ status: "success", message: "Verification successful" })
-  //   );
-  // } catch (error) {
-  //   console.log(JSON.stringify({ status: "error", message: error }));
-  // }
+  try {
+    await hre.run("verify:verify", {
+      address: deployedContractAddress,
+      constructorArguments: [totalSupply, marketingAddress],
+    });
+    //на крайняк можно текстовый файл билдить с уникальным название контракта
+    console.log(
+      JSON.stringify({ status: "success", message: "Verification successful" })
+    );
+  } catch (error) {
+    console.log(JSON.stringify({ status: "error", message: error }));
+  }
 }
 
 main().catch((error) => {
